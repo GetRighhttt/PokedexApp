dc0126a2acbd3793e65474d62d06b4b0578c4163
Stefan Bayne <stefanbayne@gmail.com>
2022-11-23 10:23:33 -0500

    First commit.

---
+++ PokedexApp/PokedexApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokedexApp
+//
+//  Created by Stefan Bayne on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokedexApp/PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Stefan Bayne on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
